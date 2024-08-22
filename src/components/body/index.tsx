f77a45a3db7180143a1a54ff19b5ef72997ce390
Brody-Schwartz <bschwartz@caxy.com>
2024-08-22 11:59:01 -0500

    running with emotion styled

---
+++ src/components/body/index.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+const Body = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100vw;
+`
+
+export default Body
---
+++ src/themes/index.tsx
@@ -0,0 +1,6 @@
+const lightTheme = {
+    primaryColor: "#990000",
+    secondaryColor: "#FFC72C",
+    backgroundColor: "#FFFFFF",
+    textColor: "#A0A006"
+}
