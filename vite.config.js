cc2b5b5b14dbff126751538302dc063513ca07dc
Brody-Schwartz <bschwartz@caxy.com>
2024-08-29 09:40:31 -0500

    added gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/jack-hallinan.github.io",
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
